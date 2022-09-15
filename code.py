98649d10fe4d17e3b60b89cd977f71a585449a57
INSORTIAL <renwell2099@gmail.com>
2022-09-15 12:24:22 -0700

    Updated code

--- code.py
+++ code.py
@@ -1,7 +1,7 @@
 from machine import Pin
 import micropython
 import time
-import random
+from mfrc522 import MFRC522
 
 '''
 The state variable keeps track of the current state of the device.
@@ -16,6 +16,10 @@ State 3: No Card - Cleanup
 state = 0
 cardIn = False
 success = False
+target = 209451056 
+reader = MFRC522(spi_id=0,sck=2,miso=4,mosi=3,cs=1,rst=0)
+start = time.time()
+reader.init()
 
 # when the pin changes, set cardin to true if switch pressed down, false otherwise
 def cardChange(pinNum):
@@ -23,22 +27,38 @@ def cardChange(pinNum):
     cardIn = True if limit_switch.value() else False
 
 #limit switch on Pin 14 triggers interrupt on rising or falling edge
-#limit_switch = Pin(14, Pin.IN, Pin.PULL_DOWN)
-limit_switch = Pin(14, Pin.IN)
+limit_switch = Pin(14, Pin.IN, Pin.PULL_DOWN)
+#limit_switch = Pin(14, Pin.IN)
 limit_switch.irq(handler=cardChange, trigger = Pin.IRQ_RISING | Pin.IRQ_FALLING)
 
+outputBit = Pin(15, Pin.OUT)
+outputBit.value(0)
+
 def read():
-    print('Read')
-    return True
+    hits = 0
+    for i in range(5):
+        (stat, tag_type) = reader.request(reader.REQIDL)
+        if stat == reader.OK:
+            (stat, uid) = reader.SelectTagSN()
+            if stat == reader.OK:
+                card = int.from_bytes(bytes(uid),"little",False)
+                print("CARD ID: "+str(card))
+                if (card == target):
+                    hits += 1 
+    result = True if (hits >= 3) else False
+    
+    if result:
+        outputBit.value(1)
+    
+    return result
 
 def cleanup():
     print('Clean')
 
-start = time.time()
-
 while True:
     print(time.time() - start)
-    time.sleep(0.1)
+    time.sleep(0.05)
+    
     if cardIn:
         if state == 0:
             state = 1
@@ -51,6 +71,7 @@ while True:
             print("This should not happen uh oh.")
             
     else:
+        outputBit.value(0)
         if success:
             state = 3
         else:
